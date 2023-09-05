03641f9e84ac329ee019b8ce9c33595a99ec1594
yenn222 <20221038@edu.hanbat.ac.kr>
2023-09-05 17:09:47 +0900

    chap01

---
+++ chap01/src/CircleArea.java
@@ -0,0 +1,13 @@
+
+public class CircleArea {
+
+	public static void main(String[] args) {
+		final double PI = 3.141592; //상수 
+		double radius = 10.2;
+		double circleArea = radius * radius * PI;
+		
+		System.out.print("반지름 " + radius + ", ");
+		System.out.println("원의 면적 = " + circleArea);
+	}
+	
+}
